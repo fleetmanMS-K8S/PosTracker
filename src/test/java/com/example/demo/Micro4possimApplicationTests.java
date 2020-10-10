cc8d6c7c087be638fbd46b25deb15b0c26102b9a
manish978973 <manishrchandran@gmail.com>
2020-10-10 04:03:08 +0200

    Update Micro4possimApplicationTests.java

--- src/test/java/com/example/demo/Micro4possimApplicationTests.java
+++ src/test/java/com/example/demo/Micro4possimApplicationTests.java
@@ -1,13 +1,1 @@
-package com.example.demo;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class Micro4possimApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
